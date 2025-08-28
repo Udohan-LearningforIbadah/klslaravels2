f61d4e63c279a7e028b1d976f5dedc5c89ed6120
Udohan-LearningforIbadah <gogofathanl3arn@gmail.com>
2025-08-28 10:38:55 +0300

    sampai sini udah bisa masuk ke Dashboard

---
+++ README.txt
@@ -0,0 +1,4 @@
+php artisan make:filament-user
+             Name: admin
+    Email address: a@a.a
+    Password:1234567890
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Filament\Panel;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
